2d11ee21b364c980261e75ca4e00cb892c664991
Ronak <ronakonline1@gmail.com>
2020-05-05 17:34:33 +0530

    TravelStyle Frontend View Created

---
+++ application/controllers/Travelstyle.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Travelstyle extends CI_Controller {
+
+
+	public function index()
+	{
+		$data = frontend_data();
+		$this->load->view('travelstyle',$data);
+	}
+}
---
+++ application/views/travelstyle.php
@@ -0,0 +1,312 @@
+<?php $this->load->view('css-links'); ?>
+<?php $this->load->view('header'); ?>
+
+<!--************************************
+			Inner Banner Start
+	*************************************-->
+<section class="tg-parallax tg-innerbanner" data-appear-top-offset="600" data-parallax="scroll" data-image-src="images/parallax/bgparallax-05.jpg">
+	<div class="tg-sectionspace tg-haslayout">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<h1>Shop</h1>
+					<h2>Donec id elit non mi porta gravida at eget metus</h2>
+					<ol class="tg-breadcrumb">
+						<li><a href="javascript:void(0);">Home</a></li>
+						<li class="tg-active">Shop</li>
+					</ol>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--************************************
+		Inner Banner End
+*************************************-->
+<!--************************************
+		Main Start
+*************************************-->
+<main id="tg-main" class="tg-main tg-sectionspace tg-haslayout">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div id="tg-content" class="tg-content">
+					<div class="tg-sectiontitle">
+						<h2>Latest Products</h2>
+					</div>
+					<div class="tg-listing tg-shopgrid">
+						<div class="row">
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-01.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-02.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-03.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-04.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-05.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-06.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-07.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-08.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+							<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-product">
+									<figure><a href="shopdetail.html"><img src="images/products/img-09.jpg" alt="image destinations"></a></figure>
+									<div class="tg-productcontent">
+										<div class="tg-producttitle">
+											<h3><a href="shopdetail.html">Simple Fabric Bag</a></h3>
+										</div>
+										<div class="tg-description">
+											<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh...</p>
+										</div>
+										<div class="tg-productfoot">
+											<div class="tg-durationrating">
+												<span class="tg-wishlist">Wishlist</span>
+												<span class="tg-stars"><span></span></span>
+												<em>(3 Review)</em>
+											</div>
+											<div class="tg-pricearea">
+												<del>$2,800</del>
+												<h4>$2,500</h4>
+											</div>
+										</div>
+									</div>
+									<a class="tg-btnaddtocart" href="javascript:void(0);">
+										<i class="icon-icons240"></i>
+										<span>add to bag</span>
+									</a>
+								</div>
+							</div>
+						</div>
+						<nav class="tg-pagination">
+							<ul>
+								<li class="tg-active"><a href="javascript:void(0);">1</a></li>
+								<li><a href="javascript:void(0);">2</a></li>
+								<li><a href="javascript:void(0);">3</a></li>
+								<li><a href="javascript:void(0);">4</a></li>
+								<li class="tg-nextpage"><a href="javascript:void(0);"><i class="fa fa-angle-right"></i></a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+<!--************************************
+		Main End
+*************************************-->
+
+<?php $this->load->view('footer'); ?>
+<?php $this->load->view('js-links');?>
