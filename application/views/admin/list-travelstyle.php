2725220b203d253c475871cb0223bbfc0a5d3640
yash-daiya <daiyayash1218@gmail.com>
2020-05-06 20:23:55 +0530

    fixed the merge issue

--- application/views/admin/list-travelstyle.php
+++ application/views/admin/list-travelstyle.php
@@ -91,7 +91,7 @@
                                                 <tr>
                                                     <td><?php echo $travel->name; ?></td>
 													<td><img src="<?php echo base_url('uploads/images/destination/').$travel->bannerimg;?>" height="150" width="200" alt="no Image"> </td>
-                                                    <td><a href=""><li class="fa fa-edit p-2 text-info" style="font-size: 30px"></li></a>
+                                                    <td><a href="update_travelstyle"><li class="fa fa-edit p-2 text-info" style="font-size: 30px"></li></a>
                                                         <a href="<?php echo base_url('admin/Destination/delete_travelstyle/').$travel->id;?>"><li class="fa fa-trash p-2 text-danger" style="font-size: 30px"></li></a>
                                                         
                                                     </td>
