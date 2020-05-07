0ff2310df4677f9bf259ee0761062bea8a3c4974
yash-daiya <daiyayash1218@gmail.com>
2020-05-07 10:20:47 +0530

    organize the controller and model(Destination)

--- application/models/DestinationM.php
+++ application/models/DestinationM.php
@@ -7,12 +7,28 @@ class DestinationM extends CI_Model{
 		$this->load->database();
 	}
 
+	//Insert Parent
 	public function insert_parent($name,$pic){
 		$this->load->database();
 		$q = $this->db->query('insert into Destinations_Parents values(null,"'.$name.'","'.$pic.'",0)');
 		return $q;
 	}
 
+	//Show parent(DropDown)
+	public function show_parent(){
+		$this->load->database();
+		$q = $this->db->query("SELECT id,name from Destinations_Parents where isdeleted = 0");
+		return $q->result();
+	}
+
+	//List Parent
+	public function listparents(){
+		$this->load->database();
+		$q = $this->db->query("SELECT * from Destinations_Parents where isdeleted = 0");
+		return $q->result();
+	}
+
+	//Delete Parent
 	public function delete_parent($id){
 		$this->load->database();
 		$q = $this->db->query('update Destinations_Parents set isdeleted=1 where id='.$id);
@@ -20,31 +36,27 @@ class DestinationM extends CI_Model{
 		return $q;
 	}
 
-	public function delete_destination($id){
-		$this->load->database();
-		$q = $this->db->query('update destination set isdeleted=1 where id='.$id);
-		$q = $this->db->query('update tours set isdeleted=1 where did='.$id);
-		return $q;
-	}
-
-	public function show_parent(){
-		$this->load->database();
-		$q = $this->db->query("SELECT id,name from Destinations_Parents where isdeleted = 0");
+	//Edit_List Parent
+	public function editlist_parent($id){
+		$q = $this->db->query("select * from destinations_parents where id = $id AND isdeleted = 0");
 		return $q->result();
 	}
 
-	public function listparents(){
-		$this->load->database();
-		$q = $this->db->query("SELECT * from Destinations_Parents where isdeleted = 0");
-		return $q->result();
+	//Update Parent
+	public function update_parent($data,$img=null){
+		if ($img==null) {
+			$q = $this->db->query('update destinations_parents set name= "'.$data['name'].'" where id = "'.$data['id'].'" AND isdeleted = 0');
+			return $q;
+		}
+		else{
+			$q = $this->db->query('update destinations_parents set name= "'.$data['name'].'", bannerimg = "'.$img.'" where id = "'.$data['id'].'" AND isdeleted = 0');
+			return $q;
+		}
 	}
 
-	public function show_destination(){
-		$this->load->database();
-		$q = $this->db->query("SELECT id,name from destination where isdeleted = 0");
-		return $q->result();
-	}
+	
 
+	//Insert Destionation
 	public function insert_destination($data,$pic){
 		$this->load->database();
 		if (isset($pic['picture3'])) {
@@ -57,23 +69,43 @@ class DestinationM extends CI_Model{
 		}
 	}
 
+	//Show Destination(DropDown)
+	public function show_destination(){
+		$this->load->database();
+		$q = $this->db->query("SELECT id,name from destination where isdeleted = 0");
+		return $q->result();
+	}
+
+	//List DEstination
 	public function listdestinations(){
 		$this->load->database();
 		$q = $this->db->query("SELECT * from destination where isdeleted = 0");
 		return $q->result();
 	}
 
+	//All Destination(Data Table)
 	public function alldestinations(){
 		$this->load->database();
 		$q = $this->db->query("SELECT destination.id, destination.name ,destinations_parents.name as pname  from destination,destinations_parents where destination.parent = destinations_parents.id  AND destinations_parents.isdeleted = 0 AND destination.isdeleted = 0");
 		return $q->result();
 	}
 
+	//Delete Destination
+	public function delete_destination($id){
+		$this->load->database();
+		$q = $this->db->query('update destination set isdeleted=1 where id='.$id);
+		$q = $this->db->query('update tours set isdeleted=1 where did='.$id);
+		return $q;
+	}
+
+
+	//Insert OverView
 	public function insert_overview($data){
 		$this->load->database();
 		$q = $this->db->query('insert into overview values(null,"'.$data['destinationname'].'","'.$data['oleft'].'","'.$data['oright'].'",0)');
 	}
 
+	//Insert FAQ
 	public function insert_faq($data){
 		$this->load->database();
 		$total = count($data['question']);
@@ -92,6 +124,7 @@ class DestinationM extends CI_Model{
 		return $q;
 	}
 
+	//Insert Packing
 	public function insert_packing($data){
 		$this->load->database();
 		$item1count = count($data['item1']);
@@ -137,12 +170,14 @@ class DestinationM extends CI_Model{
 		return $q;
 	}
 
+	//Insert SEOTags
 	public function insert_seotags($data){
 		$this->load->database();
 		$q = $this->db->query('insert into seotag values(null,"'.$data['destinationname'].'","'.$data['tags'].'",0)');
 		return $q;
 	}
 
+	//Insert Gallery
 	public function insert_gallery($data){
 		$this->load->database();
 		$qu = 'insert into gallery values';
@@ -159,42 +194,42 @@ class DestinationM extends CI_Model{
 	}
 
 
+
+	//Get Dest(Helper)
 	public function getdest($id){
 		$q = $this->db->query("select * from destination where id = $id AND isdeleted = 0");
 		return $q->result();
 	}
+	//Get Gallert(Helper)
 	public function getgallery($id){
 		$q = $this->db->query("select * from gallery where destid =$id AND isdeleted = 0");
 		return $q->result();
 	}
+	//Get OverView(Helper)
 	public function getoverview($id){
 		$q = $this->db->query("select * from overview where destid =$id AND isdeleted = 0");
 		return $q->result();
 	}
+	//Get FAQ(helper)
 	public function getfaq($id){
 		$q = $this->db->query("select * from faq where destid =$id AND isdeleted = 0");
 		return $q->result();
 	}
+	//Get packing(Helper)
 	public function getpacking($id){
 		$q = $this->db->query("select * from packing where destid =$id AND isdeleted = 0");
 		return $q->result();
 	}
-	public function editlist_parent($id){
-		$q = $this->db->query("select * from destinations_parents where id = $id AND isdeleted = 0");
+	//Get Destination(Helper)
+	public function gettoursbydestination($id){
+		$q = $this->db->query("select * from destination where isdeleted = 0");
 		return $q->result();
 	}
 
-	public function update_parent($data,$img=null){
-		if ($img==null) {
-			$q = $this->db->query('update destinations_parents set name= "'.$data['name'].'" where id = "'.$data['id'].'" AND isdeleted = 0');
-			return $q;
-		}
-		else{
-			$q = $this->db->query('update destinations_parents set name= "'.$data['name'].'", bannerimg = "'.$img.'" where id = "'.$data['id'].'" AND isdeleted = 0');
-			return $q;
-		}
-	}
+	
 
+
+	//Insert Travel Style
 	public function insert_travelstyle($data){
 		$qu = 'insert into travelstyle values(null,"[';
 		$total = count($data['destinations']);
@@ -209,16 +244,13 @@ class DestinationM extends CI_Model{
 		return $q;
 	}
 
+	//List Travel Style
 	public function listtravelstyle(){
 		$q = $this->db->query("select * from travelstyle where isdeleted = 0");
 		return $q->result();
 	}
 
-	public function gettoursbydestination($id){
-		$q = $this->db->query("select * from destination where isdeleted = 0");
-		return $q->result();
-	}
-
+	//Delete Travel Style
 	public function delete_travelstyle($id){
 		$this->load->database();
 		$q = $this->db->query('update travelstyle set isdeleted=1 where id='.$id);
